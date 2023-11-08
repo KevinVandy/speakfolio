a4dbfcd735624fd829f3953aed6e3f2efd00bbff
Kevin Van Cott <kevinvandy656@gmail.com>
2023-11-08 12:15:09 -0600

    refactor routes to folders

--- app/hooks/queries/useFetchProfile.ts
+++ app/hooks/queries/useFetchProfile.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useRouteLoaderData } from "@remix-run/react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { type loader } from "~/routes/profile.$username";
+import { type loader } from "~/routes/profile.$username/route";
 
 interface Params {
   username?: string;
@@ -20,12 +20,10 @@ export function useFetchProfile({ username }: Params) {
     );
   }, [loaderData]);
 
-  const query = useQuery({
+  return useQuery({
     initialData: loaderData,
     queryFn: () =>
       fetch(routeUrl).then((res) => res.json()) as Promise<typeof loaderData>,
     queryKey: [routeId, routeUrl],
   });
-
-  return query;
 }
